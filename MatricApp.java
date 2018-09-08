31df23d6617b7a2bd3a250f2d6c97d262435f276
jesslyn <jesslyn.chew.s.y@gmail.com>
2018-09-09 01:29:35 +0800

    Create MatricApp.java

---
+++ MatricApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MatricApp {
+	public static void main(String args[]) {
+		
+		Matric data = new Matric();
+		
+		while (true) {
+			data.printHashTable();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Insert option");
+			int option = sc.nextInt();
+			System.out.println("Insert key");
+			String key = sc.next();
+			switch (option) {
+			case 1: data.insertKey(key);
+					break;
+			case 2: data.searchKey(key);
+					break;
+			case 3: data.deleteKey(key);
+					break;
+			default: System.out.println("Invalid option");
+			}
+		}
+	}
+}
