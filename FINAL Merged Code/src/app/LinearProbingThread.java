fec215a4f6684f6695fa641a4cf2503a5f004b5b
Cheryl Chan <cherylchantr@gmail.com>
2018-09-16 23:58:48 +0800

    forgot the linear probing thread .java

---
+++ FINAL Merged Code/src/app/LinearProbingThread.java
@@ -0,0 +1,36 @@
+package app;
+
+import linearProbing.LinearProbing;
+public class LinearProbingThread extends Thread
+{
+	
+	public String id;
+	
+	public LinearProbingThread(String s) 
+	{
+		this.setName(s);
+	}
+	
+	@Override
+	public void run()
+	{
+		long cpuTime = 0;
+		
+		int index = LinearProbing.searchKey(id); //change searchkey (and several other methods) to static for access
+		if (index == -1)
+			System.out.println("No Key Found");
+		else
+			System.out.println("Key found at index: " + index);
+		
+		
+		while (cpuTime == 0)
+			cpuTime = LinearProbing.getCpuTime(this);
+		
+		System.out.println("CPU time taken (microseconds): " + cpuTime/100000);
+		
+		return;
+	}
+}
+
+
+
