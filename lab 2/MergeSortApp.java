5af80287a17211a884629f1350cdc8dba8f65c69
jesslyn <jesslyn.chew.s.y@gmail.com>
2018-09-28 11:50:25 +0800

    Update MergeSortApp.java

--- lab 2/MergeSortApp.java
+++ lab 2/MergeSortApp.java
@@ -1,8 +1,9 @@
-
 public class MergeSortApp {
 	public static void main(String[] args) {
 		MergeSort array = new MergeSort();
 		array.generateData(1);
+		array.printArray();
+		System.out.println();
 		array.mergeMethod(0, array.SIZE-1);
 		array.printArray();
 	}
