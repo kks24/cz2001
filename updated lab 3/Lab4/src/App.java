cb7732b0a75ce742948e19b20e842d66388ea426
pinkai123 <kaiz123129@gmail.com>
2018-11-04 11:00:42 +0800

    Changes

--- updated lab 3/Lab4/src/App.java
+++ updated lab 3/Lab4/src/App.java
@@ -12,15 +12,20 @@ public class App {
 			System.out.println("1. Select vertex size");
 			System.out.println("2. Select shortest path");
 			System.out.println("3. Quit");
+			option = sc.nextInt();
 			switch (option) {
 				case 1:
 					System.out.println("Choose number of cities: 8 / 13 / 18");
 					int citySize = sc.nextInt();
-					if (citySize != 8 | citySize != 13 | citySize != 18)
+					if (citySize == 8 || citySize == 13 || citySize == 18) {
+						graph = new Graph2(citySize);
+						graphFlag = true;
 						break;
-					graph = new Graph2(citySize);
-					graphFlag = true;
-					break;
+					}
+					else {
+						System.out.println("Please input the correct number of cities");
+						continue;
+					}
 				case 2:
 					if (graphFlag == false)
 						break;
@@ -29,6 +34,7 @@ public class App {
 					System.out.println("Input destination city name:");
 					String destinationName = sc.next();
 					graph.BFS(sourceName, destinationName);
+					continue;
 				case 3: 
 					System.out.println("Shutting down");
 				default:
